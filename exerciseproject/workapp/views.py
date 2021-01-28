2ca1e81b40e7cb3fefd8a819c58cf5e456c4976e
jinsujeong <ejd090393@gmail.com>
2021-01-28 22:55:04 +0900

    4주차 파일 업로드

--- exerciseproject/workapp/views.py
+++ exerciseproject/workapp/views.py
@@ -10,9 +10,24 @@ def exercise1(request):
 
 def exercise2(request) :
     if request.method == 'POST':
-        name = request.POST['name']
-        opinion = request.POST['opinion']
-        context = {'name': name, 'opinion': opinion}
+        na = request.POST['name']
+        op = request.POST['opinion']
+        context = {'na': na, 'op': op}
     else :
-         context = None
+         context = None  # 탬플릿에 전달할 게 없으므로 None 전달
     return render(request, 'exercise2.html', context)
+
+def product1(request) :
+    template = loader.get_template('product1.html')
+    return HttpResponse(template.render(None, request))
+
+def basket1(request):
+    query = 'pid' in request.GET
+    if query :
+        selection = request.GET['pid']
+    result = selection.count('p')
+    context = {
+        'selection' : selection,
+        'result': result,
+    }
+    return render(request, 'basket1.html', context)
